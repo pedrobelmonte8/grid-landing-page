b376636a2f25e62e4ddb8b5f3acd42e5f73da1a4
Pedro <pedrojuan36022@gmail.com>
2026-08-16 17:11:06 +0200

    Añadidos efectos para pseudo-clases focus y hover y cambio de icono al abrir el menú

--- assets/src/scripts/script.js
+++ assets/src/scripts/script.js
@@ -3,6 +3,7 @@ const menuIcon = document.querySelector("#menu-icon");
 menuIcon.addEventListener("click", () => {
   const menu = document.querySelector("#menu");
   const overlay = document.getElementsByClassName("overlay")[0];
+  menuIcon.src = `./assets/images/${menu.classList.contains("open") ? "icon-menu" : "icon-close"}.svg`;
   menu.classList.toggle("open");
   overlay.classList.toggle("open");
 });
